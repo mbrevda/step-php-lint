94e5e95e415f39bf59627eeace5bff741bf1c421
Vincent Gijsen <vtj.gijsen@gmail.com>
2013-11-25 13:32:47 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+# RSync deployment step
+Verifies all *.php files in your specified directory
+
+
+# Options
+
+* `directory` the remote directory to upload to
+
+# Example
+
+    - phplint-build:
+        directory: $HOME/
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+if [ -z "$WERCKER_PHPLINT_BUILD_DIRECTORY" ]
+then
+    fail 'missing directory option, please add this the rsync-deploy step in wercker.yml'
+fi
+
+if hash php 2>/dev/null; then
+	info 'php is found'
+else
+   	fail 'missing php executable!'
+fi
+
+
+result=$(find  "$WERCKER_PHPLINT_BUILD_DIRECTORY" -name \*.php -exec echo  php {} \;)
+if [[ $RESULT != "0" ]] 
+ warning $result;
+ fail 'php lint-check failed';
+fi
+
+info 'completed lint-check';
