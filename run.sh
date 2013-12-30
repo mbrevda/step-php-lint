5694bf854ce4853c88f6efc3ece00aa01c05f84e
Moshe Brevda <mbrevda@gmail.com>
2013-12-30 21:21:21 +0200

    Try setting debug

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -x
+#!/bin/bash 
+set -x
 if [ -z "$WERCKER_PHP_LINT_DIRECTORY" ];then
     fail 'missing "directory" option, please add this to the php-lint step in your wercker.yml'
 fi
